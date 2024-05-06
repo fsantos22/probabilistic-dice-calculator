3058285e94908dbe4f3702417c7c146aa3a8449b
fsantos22 <22fsantos@gmail.com>
2024-05-06 14:44:40 -0300

    feat: add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { inject } from "@vercel/analytics"
+import { Analytics } from "@vercel/analytics/react";
+
+inject()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
+    <Analytics/>
     <App />
   </>
 );
